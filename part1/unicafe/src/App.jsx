d9b66ccb188a83252304b994c6235f6f28b58cb5
Klaus Mustonen <klaus.mustonen@aalto.fi>
2025-08-27 18:41:57 +0300

    unicafe step2

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <div>
+        <h2>give feedback</h2>
+        <button onClick={() => setGood(good + 1)}>good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+        <button onClick={() => setBad(bad + 1)}>bad</button>
+      </div>
+      <div>
+        <h2>statistics</h2>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>average {(good * 1 - bad * 1) / (good + neutral + bad)}</p>
+        <p>positive {100 * good / (good + neutral + bad)}%</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
