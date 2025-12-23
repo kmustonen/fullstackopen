f214516298c6f4d59f9410e2fd8ad9a8e8b19218
Klaus Mustonen <klaus.mustonen@aalto.fi>
2025-12-23 14:38:41 +0200

    add 3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -39,6 +39,13 @@ app.get('/api/persons/:id', (req, res) => {
     }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+})
+
 app.get('/api/info', (req, res) => {
     res.send(`
         <p>Phonebook has info for ${persons.length} people</p>
