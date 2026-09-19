9140b6e3e4dfd4a5d32a54f1a18f8aeb2bb1e3c5
Klaus Mustonen <klaus.mustonen21@gmail.com>
2026-09-19 17:12:39 +0300

    add 3.12

---
+++ part3/phonebook/backend/mongo.js
@@ -0,0 +1,49 @@
+//process.loadEnvFile();
+
+const mongoose = require('mongoose')
+//const db_username = process.env.DB_USERNAME
+//const db_password = process.env.DB_PASSWORD
+
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstackopen:${password}@fullstackopen.1lfkm6u.mongodb.net/phonebook?appName=fullstackopen`
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url, { family: 4 })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length == 5){
+
+  const name = process.argv[3]
+  const phone = process.argv[4]
+
+  const person = new Person({
+    name: name,
+    phone: phone,
+  })
+
+  person.save().then(result => {
+    console.log(`added ${name} number ${phone} to phonebook`)
+    mongoose.connection.close()
+  })
+} 
+else if (process.argv.length == 3){
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person)
+    })
+    mongoose.connection.close()
+  })
+}
