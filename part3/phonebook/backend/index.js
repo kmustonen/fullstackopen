a5e12f0d425e7636618d89aa36764816bc336d60
Klaus Mustonen <klausmustonen21@gmail.com>
2026-09-19 19:07:06 +0300

    add 3.19

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -103,7 +103,10 @@ const errorHandler = (error, req, res, next) => {
 
     if (error.name === 'CastError') {
         return res.status(400).json({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return res.status(400).json({ error: error.message })
     }
+
 
     next(error)
 }
