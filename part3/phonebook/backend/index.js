5a03d2b4153f0ef28324750ea1397a6b36fcbb63
Klaus Mustonen <klaus.mustonen21@gmail.com>
2026-09-19 18:17:15 +0300

    add 3.15

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -46,10 +46,11 @@ app.get('/api/persons/:id', (req, res) => {
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = req.params.id
-    persons = persons.filter(person => person.id.toString() !== id)
-
-    res.status(204).end()
+    Person.findByIdAndDelete(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res) => {
