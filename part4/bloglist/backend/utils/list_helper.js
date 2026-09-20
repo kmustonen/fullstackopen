5ccf25282741aaa9fdbb23d64b2cb2c09bed7c03
Klaus Mustonen <klausmustonen21@gmail.com>
2026-09-20 22:53:03 +0300

    fix formatting

--- part4/bloglist/backend/utils/list_helper.js
+++ part4/bloglist/backend/utils/list_helper.js
@@ -7,23 +7,23 @@ const totalLikes = (blogs) => {
 const favoriteBlog = (blogs) => {
   if (blogs.length === 0) return null
 
-  return blogs.reduce((prev, current) => prev.likes > current.likes ? prev : current);
+  return blogs.reduce((prev, current) => prev.likes > current.likes ? prev : current)
 }
 
 const mostBlogs = (blogs) => {
-    if (blogs.length === 0) return null
+  if (blogs.length === 0) return null
 
-    const counts = lodash.countBy(blogs, 'author')
-    const author = lodash.maxBy(Object.keys(counts), (a) => counts[a])
-    return { author: author, blogs: counts[author] }
+  const counts = lodash.countBy(blogs, 'author')
+  const author = lodash.maxBy(Object.keys(counts), (a) => counts[a])
+  return { author: author, blogs: counts[author] }
 }
 
 const mostLikes = (blogs) => {
-    if (blogs.length === 0) return null
+  if (blogs.length === 0) return null
 
-    const blogsPerAuthor = lodash.groupBy(blogs, 'author')
-    const author = lodash.maxBy(Object.keys(blogsPerAuthor), (a) => totalLikes(blogsPerAuthor[a]))
-    return { author: author, likes: totalLikes(blogsPerAuthor[author])}
+  const blogsPerAuthor = lodash.groupBy(blogs, 'author')
+  const author = lodash.maxBy(Object.keys(blogsPerAuthor), (a) => totalLikes(blogsPerAuthor[a]))
+  return { author: author, likes: totalLikes(blogsPerAuthor[author]) }
 }
 
 module.exports = {
