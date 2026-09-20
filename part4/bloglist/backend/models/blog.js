7db2db43891be239499b4af7510eafd22e3f3ffa
Klaus Mustonen <klausmustonen21@gmail.com>
2026-09-20 21:08:31 +0300

    add 4.2

---
+++ part4/bloglist/backend/models/blog.js
@@ -0,0 +1,11 @@
+const config = require('../utils/config')
+const mongoose = require('mongoose')
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
