5238167c9c2e39c912d36fb4a23d707e9d7f5b5e
Klaus Mustonen <klausmustonen21@gmail.com>
2026-09-21 20:34:19 +0300

    add 4.8

---
+++ part4/bloglist/backend/tests/blogs_api.test.js
@@ -0,0 +1,34 @@
+const assert = require('node:assert')
+const { test, after, beforeEach, describe } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./test_helper')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+describe('when there are initially some blogs saved', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
+  })
+
+  test('notes are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all notes are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  })
+})
+
+after(async () => {
+  await Blog.deleteMany({})
+  await mongoose.connection.close()
+})
--- part4/bloglist/backend/tests/list_helper.test.js
+++ part4/bloglist/backend/tests/list_helper.test.js
@@ -1,57 +1,7 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
-
-const blogs = [
-  {
-    _id: '5a422a851b54a676234d17f7',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: '5a422b3a1b54a676234d17f9',
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-    __v: 0
-  },
-  {
-    _id: '5a422b891b54a676234d17fa',
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-    __v: 0
-  },
-  {
-    _id: '5a422ba71b54a676234d17fb',
-    title: 'TDD harms architecture',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-    likes: 0,
-    __v: 0
-  },
-  {
-    _id: '5a422bc61b54a676234d17fc',
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    likes: 2,
-    __v: 0
-  }
-]
+const helper = require('./test_helper')
 
 describe('total likes', () => {
   const listWithOneBlog = [
@@ -71,14 +21,14 @@ describe('total likes', () => {
   })
 
   test('when list has multiple blogs, equals the sum of likes', () => {
-    const result = listHelper.totalLikes(blogs)
+    const result = listHelper.totalLikes(helper.initialBlogs)
     assert.strictEqual(result, 36)
   })
 })
 
 describe('favorite blog', () => {
   test('return the blog with max number of likes', () => {
-    const result = listHelper.favoriteBlog(blogs)
+    const result = listHelper.favoriteBlog(helper.initialBlogs)
     assert.deepStrictEqual(result.likes, 12)
   })
 
@@ -92,7 +42,7 @@ describe('favorite blog', () => {
 
 describe('most blogs', () => {
   test('return author with most blogs', () => {
-    const result = listHelper.mostBlogs(blogs)
+    const result = listHelper.mostBlogs(helper.initialBlogs)
     assert.deepStrictEqual(result.blogs, 3)
   })
 
@@ -106,7 +56,7 @@ describe('most blogs', () => {
 
 describe('most likes', () => {
   test('return author with most likes', () => {
-    const result = listHelper.mostLikes(blogs)
+    const result = listHelper.mostLikes(helper.initialBlogs)
     assert.deepStrictEqual(result.likes, 17)
   })
 
