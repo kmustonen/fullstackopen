12363c70a45996f2f0f57e67293db0fbca51f7d4
Klaus Mustonen <klausmustonen21@gmail.com>
2026-09-22 13:26:41 +0300

    add 4.15

---
+++ part4/bloglist/backend/models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  name: String,
+  password: { type: String, required: true }
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('User', userSchema)
