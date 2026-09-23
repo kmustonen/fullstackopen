c3f414868f870c804ea5d584bb29eb2588ea1df2
Klaus Mustonen <klausmustonen21@gmail.com>
2026-09-23 22:18:00 +0300

    add 5.7

--- part5/bloglist/frontend/src/components/Blog.jsx
+++ part5/bloglist/frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [showDetails, setShowDetails] = useState(false)
+
+  return (
+  <div style={blogStyle}>
+    <div>
+      {blog.title} {blog.author} {showDetails 
+        ? <button onClick={() => setShowDetails(false)}>hide</button>
+        : <button onClick={() => setShowDetails(true)}>view</button>}
+    </div>
+    {showDetails && <div>
+    <div>
+      {blog.url}
+    </div> 
+    <div>
+      Likes: {blog.likes} <button>like</button>
+    </div>
+    <div>
+      {blog.user.name}
+    </div>
+    </div>} 
   </div>  
-)
+  )
+}
 
 export default Blog
