70a7de7786a32237abf96895223e38234d3612f8
Klaus Mustonen <klausmustonen21@gmail.com>
2026-09-26 02:26:42 +0300

    add notification helper

--- part5/bloglist/frontend/src/App.jsx
+++ part5/bloglist/frontend/src/App.jsx
@@ -18,10 +18,7 @@ const App = () => {
   const navigate = useNavigate()
 
   const [blogs, setBlogs] = useState([])
-  const [message, setMessage] = useState({
-    message: null,
-    status: null
-  })
+  const [message, setMessage] = useState({ message: null, status: null })
   const [user, setUser] = useState(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedBloglistappUser')
     if (!loggedUserJSON) return null
@@ -32,26 +29,24 @@ const App = () => {
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs.sort(function(a,b) {return b.likes - a.likes}) )
-    )
+      setBlogs(blogs.sort(function(a,b) {return b.likes - a.likes})))
   }, [])
+
+  const sendNotification = async (message, status) => {
+    setMessage({ message: message, status: status })
+    setTimeout(() => {setMessage({ message: null, status: null })}, 5000)
+  }
 
   const handleLogin = async ({ username, password }) => {
     try {
       const user = await loginService.login({ username, password })
-
-      window.localStorage.setItem(
-        'loggedBloglistappUser', JSON.stringify(user)
-      )
+      window.localStorage.setItem('loggedBloglistappUser', JSON.stringify(user))
 
       blogService.setToken(user.token)
       setUser(user)
       return true
     } catch {
-      setMessage({ message: 'wrong username or password', status: 'error' })
-      setTimeout(() => {
-        setMessage({ message: null, status: null })
-      }, 5000)
+      sendNotification('wrong username or password', 'error')
       return false
     }
   }
@@ -63,30 +58,13 @@ const App = () => {
   }
 
   const createBlog = async (blogObject) => {
-    if (!blogObject.title || !blogObject.url) {
-      setMessage({ message: 'title and url are required', status: 'error' })
-      setTimeout(() => {
-        setMessage({ message: null, status: null })
-      }, 5000)
-      return
-    }
-
     try {
       const returnedBlog = await blogService.create(blogObject)
       setBlogs(blogs.concat({ ...returnedBlog, user }))
-      setMessage({
-        message: `a new blog ${blogObject.title} by ${blogObject.author} was added`,
-        status: 'success'
-      })
-      setTimeout(() => {
-        setMessage({ message: null, status: null })
-      }, 5000)
+      sendNotification(`a new blog ${blogObject.title} by ${blogObject.author} was added`, 'success')
       navigate('/')
     } catch {
-      setMessage({ message: 'error in adding new blog', status: 'error' })
-      setTimeout(() => {
-        setMessage({ message: null, status: null })
-      }, 5000)
+      sendNotification('error in adding new blog', 'error')
     }
   }
 
@@ -102,13 +80,7 @@ const App = () => {
   const handleRemove = async (blogObject) => {
     if (window.confirm(`remove blog ${blogObject.title} by ${blogObject.author}`)) {
       await blogService.remove(blogObject)
-      setMessage({
-        message: `blog ${blogObject.title} by ${blogObject.author} was removed`,
-        status: 'success'
-      })
-      setTimeout(() => {
-        setMessage({ message: null, status: null })
-      }, 5000)
+      sendNotification(`blog ${blogObject.title} by ${blogObject.author} was removed`, 'success')
       const updatedBlogs = await blogService.getAll()
       setBlogs( updatedBlogs.sort(function(a,b) {return b.likes - a.likes}))
       navigate('/')
